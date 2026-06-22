c10369d8017df5e05d93f7a4462f74f35e8d1187
Altenus <128000396+Altenus@users.noreply.github.com>
2026-06-22 01:32:38 -0400

    Add files via upload
    My first programming projects in Python using logic, lists, and random choices.

---
+++ proyectoFinalDia1.py
@@ -0,0 +1,31 @@
+print("""Bienvenido Terricola 
+te vamos a hacer unas cuantas preguntas 
+para nuestra nueva marca de cerveza 
+""")
+
+Tu_animal_favorito = input("Ingrese tu animal: ")
+Color_favorito = input("Ingrese tu color: ")
+Tu_Nombre = input("Ingrese tu nombre: ")
+
+print(r"""
+          ( ) ( ) ( ) ( )
+        ( ) ( ) ( ) ( ) ( )
+       _____________________
+      (_____________________)
+
+       |  ___   ___   ___  |
+       | |   | |   | |   | |  ,---.
+       | |   | |   | |   | | /     \
+       | |___| |___| |___| ||       |
+       | |   | |   | |   | ||       |
+       | |   | |   | |   | | \     /
+       | |___| |___| |___| |  `---'
+       |                   |
+       \___________________/
+        \_________________/
+""")
+
+
+print(f"Este Nombre encaja perfecto con tu marca \"{Tu_Nombre} {Tu_animal_favorito} {Color_favorito}\",. ")
+
+print("\nQue Nombre mas espertacular espero que te vaya bien en tus ventas :D")
---
+++ proyecto_2.py
@@ -0,0 +1,51 @@
+import random
+print(r""" 
+••      ••  ••••••••••  ••••••••      ••••••    ••••••••••    ••••••••  
+••      ••  • ••••••••  • ••••••      ••••••    • ••••••••    ••••••••  
+••      ••  ••          ••      ••  ••      ••  ••          ••          
+••      ••  ••          ••      ••  ••      ••  ••          ••          
+• •••••• •  • ••••••    • ••••••    ••      ••  • ••••••      ••••••    
+• •••••• •  • ••••••    • •••• •    ••      ••  • ••••••      ••••••    
+••      ••  ••          ••    ••    ••      ••  ••                  ••  
+••      ••  ••          ••    ••    ••      ••  ••                  ••  
+••      ••  • ••••••••  ••      ••    ••••••    • ••••••••  ••••••••    
+••      ••  ••••••••••  ••      ••    ••••••    ••••••••••  ••••••••        
+""")
+
+print()
+
+print("Nesesitamos tu ayuda para selecionar nuestro nuevo heroe!!!   ")
+print()
+opcion1 = input("Nombre del heroe: ").title()
+opcion2 = input("Super poder principal: ").title()
+opcion3 = input("Ciudad que protege: ").title()
+opcion4 = input("Bando al que pertenece: ").title()
+
+almacen1 = [opcion1, opcion2, opcion3, opcion4]
+
+
+print(f"\nNombre del heroe: {opcion1}\n "f"\nTu poder: {opcion2}\n " f"\nCiudad que proteges: {opcion3}\n "
+      f"\nBando al que perteneces {opcion4}\n")
+dato_sorpresa = random.choice(almacen1)
+print(f"El sistema ha selecionado tu dato mas importante: {dato_sorpresa}!!")
+
+destino_Heroes = [
+      "Muere salvando al mundo y se convierte en un símbolo eterno",
+      "Deja las armas y busca una vida pacífica y oculta de la fama",
+      "Se aleja del mundo al sentir que su poder es un peligro para otros."
+]
+
+destino_Villanos = [
+      "El villano queda atrapado repitiendo el mismo evento catastrófico o doloroso por la eternidad ",
+                    "Encierro en dimensiones vacías, espejos, lámparas mágicas o el propio subconsciente ",
+                    "Pierde todo el poder y terminar en una posición ridícula, como ser el juguete olvidado en un camión de basura "
+                    ]
+print()
+
+if opcion4== "Heroe":
+      final = random.choice(destino_Heroes)
+else:
+      final =random.choice(destino_Villanos)
+
+
+print(f"\"{opcion1} {final}\" ")
