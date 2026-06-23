8e8ee782c456f2378b8fefdb38e55262f0a960c7
Altenus <128000396+Altenus@users.noreply.github.com>
2026-06-22 23:55:02 -0400

    Complete Day 2 Update: Data Types, Math Modifiers, and Advanced Logic
    - Mastered numerical data types by implementing Integers (int) and Floats (float).
    - Applied type casting to handle dynamic inputs and clean up decimal outputs.
    - Developed an interactive nested conditional system (if/elif/else) for gameplay.
    - Created dynamic game mechanics with stat pre-initialization, rewards, and penalties.
    - Upgraded the interface using multi-line f-strings and visual emoji anchors.

--- proyecto_2.py
+++ proyecto_2.py
@@ -24,8 +24,17 @@ opcion4 = input("Bando al que pertenece: ").title()
 almacen1 = [opcion1, opcion2, opcion3, opcion4]
 
 
-print(f"\nNombre del heroe: {opcion1}\n "f"\nTu poder: {opcion2}\n " f"\nCiudad que proteges: {opcion3}\n "
-      f"\nBando al que perteneces {opcion4}\n")
+print(f"""
+🦸‍Nombre del heroe: {opcion1} 
+
+🧬Tu poder: {opcion2}
+
+🏙️Ciudad que proteges: {opcion3} 
+
+🛡️/😈Bando al que perteneces {opcion4}""")
+
+print()
+
 dato_sorpresa = random.choice(almacen1)
 print(f"El sistema ha selecionado tu dato mas importante: {dato_sorpresa}!!")
 
@@ -47,9 +56,6 @@ if opcion4== "Heroe":
 else:
       final =random.choice(destino_Villanos)
 
-
-print(f"\"{opcion1} {final}\" ")
-
 print()
 
 # Day 2 I added Stats and damage calculator
@@ -62,12 +68,57 @@ multiplicador_De_Fuerza = float(input("Ingresa el multiplicador de Fuerza: "))
 print()
 Dano_Total = int(ataque_Base * multiplicador_De_Fuerza)
 
+Aumento_De_Poder = 0    # Power Up
+Perdida_De_Poder = 0    # Lost Power
 print("""
 =============================================
          FICHA DEL HEROE ACTUALIZADA        
 =============================================
 """)
 
-print(f"***ESTADISTICAS NUMERICAS***\n ->ATAQUE BASE: {ataque_Base}\n ->MULTIPLICADOR: {multiplicador_De_Fuerza}X\n "
-      f"->DANO_TOTAL: {Dano_Total} puntos.\n")
+print(f"***ESTADISTICAS NUMERICAS***\n ->⚔️ATAQUE BASE: {ataque_Base}\n ->🚀MULTIPLICADOR: {multiplicador_De_Fuerza}X\n "
+      f"->💥DANO_TOTAL: {Dano_Total} puntos.\n")
 print("===============================================")
+
+# Added nested interactive menus, dynamic power modifiers, and pre-initialized global variables.⬇️
+
+print("""
+====================================
+¿ESTAS LISTO PARA TU PRIMERA MISION?
+====================================
+""")
+
+print("1 Si")
+print("2 No")
+
+Decision = int(input("Elige una opcion 1 o 2: " ))
+if Decision == 1:
+    print("TE TELETRANSPORTASTE A UNA CUEVA")
+    print()
+    print("\033[1m--- ¡Te encuentras una puerta cerrada! ---\033[1m")
+    print()
+    print("->1 Asomarse por la puerta ")
+    print("->2 Derribar la puerta de una patada ")
+    print()
+    Decision2 = int(input("""Elige una opcion 1 o 2: """ ))
+    if Decision2 == 1:
+        print("Entraste en silencio y viste que todos estaban duermiendo ")
+        Aumento_De_Poder = Dano_Total * 30
+        print(f"🚀 Multiplicaste tu poder :D : {Aumento_De_Poder} puntos totales")
+    elif Decision2 == 2:
+        print("Despertaste a todos ")
+    else:
+        print("esta opcion no es valida")
+
+else:
+    print("Haz rechazado la Mision perdiste los posibles puntos ")
+    perdida_De_Poder = Dano_Total - 200
+
+    print(f"Perdiste Mucho poder por no aceptar la mision :C tu Nuevo Dano total es : {perdida_De_Poder} ")
+    print()
+    print(type(perdida_De_Poder))
+
+print()
+
+
+print(f"\"{opcion1} {final}\" ")
